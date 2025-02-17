c440ef0ec3114c3977aa5b42591d67d3fa414f7e
diego-smart-app <d_igo_w@hotmail.com>
2025-02-17 12:52:59 -0300

    ex

--- Aulas/Aula 13/ambiente.js
+++ Aulas/Aula 13/ambiente.js
@@ -1,8 +1,24 @@
 
+for (var c = 1; c <= 10; c++)
+{
+    console.log(`Foi ${c}`)
+}
+
+/*
+var c = 0
+
+do
+{
+    console.log(`Iniciando em ${c}`)
+    c ++
+} while(c <= 300)
+*/
+/*
 var c = 0
 
 while(c <= 300)
 {
-    c ++
     console.log(`Iniciando em ${c}`)
+    c ++
 }
+*/
