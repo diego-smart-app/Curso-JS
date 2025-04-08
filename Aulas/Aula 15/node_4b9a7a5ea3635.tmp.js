5b638378f8714cc9d9bd28be743af5c6ee1a0869
diego-smart-app <d_igo_w@hotmail.com>
2025-04-08 14:41:55 -0300

    Create node_4b9a7a5ea3635.tmp.js

---
+++ Aulas/Aula 15/node_4b9a7a5ea3635.tmp.js
@@ -0,0 +1,14 @@
+let num = [1, 2, 8, 87, 45, 87, 22, 49, 71, 47, 55, 14, 25, 17, 36, 38, 34, 28, 19, 56]
+
+num.push(65)
+
+console.log(num.length)
+
+num.sort()
+
+console.log(num[2])
+
+for (var n = 0; num.length; n++)
+{
+    console.log(num[n])
+}
